b854e39c0d888ec8767f3b5cde867d3bc8fe1f83
Ryland Quirk <quirk2580@gmail.com>
2020-11-18 19:34:05 -0600

    test call for giphy

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 //This is how to comment in javascript
-var drink = "margarita"
-var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink
+var drink = "margarita";
+var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
+var queryURLgiphy = "http://api.giphy.com/v1/gifs/search?q=" + drink + "&api_key=KzTNLUmjkNMNh8q6dfPusWKX78lyCNaV&limit=5";
 
 //testing a margarita
 $.ajax({
@@ -11,3 +12,14 @@ $.ajax({
     console.log(response.drinks[0]);
     console.log(response.drinks[0].strDrink);
   });
+
+$.ajax({
+    url: queryURLgiphy,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.data[0].url);
+  });
+
+  //Giphy API key
+  //KzTNLUmjkNMNh8q6dfPusWKX78lyCNaV
