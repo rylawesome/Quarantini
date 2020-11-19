e2deb2e66f3f816a077ae4f770fb0f28a75e6c1c
Ryland Quirk <quirk2580@gmail.com>
2020-11-18 19:02:05 -0600

    add vars test api

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,14 @@
 //This is how to comment in javascript
+var drink = "margarita"
+var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink
+
+//testing a margarita
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.drinks[0]);
+    console.log(response.drinks[0].strDrink);
+    console.log(response.drinks[0].strDrink);
+  });
