7a8cad85bab3325e9826eb263e082c42c4135972
Ryland Quirk <quirk2580@gmail.com>
2020-11-21 10:32:58 -0600

    update user search

--- Assets/script.js
+++ Assets/script.js
@@ -1,35 +1,28 @@
 //This is how to comment in javascript
-
-
-test = true;
-
-if (test === true) {
-    var drink = "margarita";
-    }
-else {
-    var drink = "";
-    };
+var drink = "";
 var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
 var queryURLgiphy = "http://api.giphy.com/v1/gifs/search?q=" + drink + "&api_key=KzTNLUmjkNMNh8q6dfPusWKX78lyCNaV&limit=5";
-var drink = "margarita";
 var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
 
-$.ajax({
-    url: queryURLgiphy,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    console.log(response.data[0].url);
-  });
+$("#run-search").on("click", function(event) {
+    event.preventDefault();
+    drink = document.getElementById("search-term").value;
+    queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(drink);
+        console.log(queryURL);
+        console.log(response);
+        console.log(response.drinks[0].strDrink);
+        document.getElementById("drinkName").innerHTML = response.drinks[0].strDrink;
+      });
+});
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    console.log(response.drinks[0].strDrink);
+  function findDrink(response) {
     document.getElementById("drinkName").innerHTML = response.drinks[0].strDrink;
-  });
+  };
 
   //Giphy API key
   //KzTNLUmjkNMNh8q6dfPusWKX78lyCNaV
